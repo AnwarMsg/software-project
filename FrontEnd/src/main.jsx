8344262b393a1deedf207f2510a4ec646b327100
Reda-Sbl <mohamed.reda.sabile@gmail.com>
2025-05-09 02:08:29 +0100

    home page, account page (client and admin), header, flying button

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { store } from './Redux/store.js'
-import { Provider } from 'react-redux'
+import { store } from './Redux/store.js';
+import { Provider } from 'react-redux';
+import { UserProvider } from './components/UserContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </Provider>
   </StrictMode>,
-)
+);
